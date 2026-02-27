2bec147c7e8fa41581e4904206d7b3d46dd1e2c3
ManimaranAlwar <amanimaran2005@gmail.com>
2026-02-27 13:12:30 +0530

    version - 9

--- app.py
+++ app.py
@@ -1,26 +1,19 @@
-from flask import Flask, render_template, request, jsonify
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# 1. Main Dashboard (Connecting all games)
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# 2. Grammar Game Route
 @app.route('/grammer')
 def grammer_game():
-    return render_template('grammer.html', puzzles=puzzles)
+    return render_template('grammer.html')
 
-# 3. Plant Growth Game Route
 @app.route('/plant_growth')
 def plant_growth():
     return render_template('plant_growth.html')
 
-# 4. Sequence Game Route
 @app.route('/sequence')
 def sequence():
     return render_template('sequence.html')
-if __name__ == '__main__':
-    app.run(debug=True, port=5001)
